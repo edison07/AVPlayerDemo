d7a227b95c23b17a17c796cc2d912673194a8451
Edison <fswedison@gmail.com>
2025-03-10 04:09:12 +0800

    Init xcode project

---
+++ GlossikaPlayer/GlossikaPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GlossikaPlayer
+//
+//  Created by Edison on 2025/3/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
